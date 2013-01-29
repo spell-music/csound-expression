5c6ad24f046002a096717130eb3241fb07322fcb
Anton Kholomiov <holomevao@quarta.su>
2013-01-29 12:22:02 +0400

    Advanced opcodes toc

---
+++ src/Csound/Opcode/Advanced.hs
@@ -0,0 +1,37 @@
+-- | Advanced Signal Processing
+module Csound.Opcode.Advanced (
+    -----------------------------------------------------
+    -- * Modulation and Distortion
+
+    -- ** Frequency Modulation
+
+    -- ** Distortion and Wave Shaping
+
+    -- ** Flanging, Phasing, Phase Shaping
+
+    -- ** Doppler Shift
+
+    -----------------------------------------------------
+    -- * Granular Synthesis
+
+    -----------------------------------------------------
+    -- * Convolution
+
+    -----------------------------------------------------
+    -- * FFT and Spectral Processing
+
+    -- ** Realtime Analysis And Resynthesis
+
+    -- ** Writing FFT Data To A File And Reading From It
+
+    -- ** FFT Info
+
+    -- ** Manipulating FFT Signals
+   
+    -----------------------------------------------------
+    -- * Physical Models and FM Instruments
+
+    -- ** Waveguide Physical Modelling
+
+    -- ** FM Instrument Models
+) where
