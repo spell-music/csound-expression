9e05b9bfa2d97e4b35f74777093eb665b81783ef
Anton Kholomiov <holomevao@quarta.su>
2013-01-29 12:36:27 +0400

    Data opcodes toc

---
+++ src/Csound/Opcode/Data.hs
@@ -0,0 +1,52 @@
+-- | Data
+module Csound.Opcode.Data (
+
+    -----------------------------------------------------
+    -- * Buffer and Function tables
+
+    -- ** Creating Function Tables (Buffers)
+
+    -- ** Writing To Tables
+
+    -- ** Reading From Tables
+
+    -- ** Saving Tables To Files
+
+    -- ** Reading Tables From Files
+
+    -----------------------------------------------------
+    -- * Signal Input and Output,  Sample and Loop Playback, Soundfonts
+
+    -- ** Signal Input And Output
+
+    -- ** Sample Playback With Optional Looping
+
+    -- ** Soundfonts And Fluid Opcodes
+
+    -----------------------------------------------------
+    -- *  File Input and Output
+
+    -- ** Sound File Input
+
+    -- ** Sound File Queries
+
+    -- ** Sound File Output
+
+    -- ** Non-Soundfile Input And Output
+
+    -----------------------------------------------------
+    -- * Converters of Data Types
+
+    -----------------------------------------------------
+    -- * Printing and Strings
+
+    -- ** Simple Printing
+
+    -- ** Formatted Printing
+
+    -- ** String Variables
+
+    -- ** String Manipulation And Conversion
+
+) where
+
