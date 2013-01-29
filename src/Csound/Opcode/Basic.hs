496eb77be86ce667e0ec9bd9cb537c02730eeea5
Anton Kholomiov <holomevao@quarta.su>
2013-01-29 12:16:07 +0400

    Basic opcodes toc

---
+++ src/Csound/Opcode/Basic.hs
@@ -0,0 +1,68 @@
+-- | Basic signal processing
+module Csound.Opcode.Basic(
+    -----------------------------------------------------
+    -- * Oscillators and phasors
+
+    -- ** Standard Oscillators
+
+    -- ** Dynamic Sprectrum Oscillators
+
+    -- ** Phasors
+
+    -----------------------------------------------------
+    -- * Random and Noise generators    
+
+    -----------------------------------------------------
+    -- * Envelopes
+
+    -----------------------------------------------------
+    -- * Delays
+
+    -- ** Audio delays
+
+    -- ** Control delays
+
+    -----------------------------------------------------
+    -- * Filters
+
+    -- ** Low Pass Filters
+
+    -- ** High Pass Filters
+
+    -- ** Band Pass And Resonant Filters
+
+    -- ** Band Reject Filters
+
+    -- ** Filters For Smoothing Control Signals
+
+    -----------------------------------------------------
+    -- * Reverb
+
+    -----------------------------------------------------
+    -- * Signal Measurement, Dynamic Processing, Sample Level Operations
+
+    -- ** Amplitude Measurement And Following
+
+    -- ** Pitch Estimation
+
+    -- ** Tempo Estimation
+
+    -- ** Dynamic Processing
+
+    -- ** Sample Level Operations
+
+    -----------------------------------------------------
+    -- * Spatialization
+
+    -- ** Panning
+
+    -- ** VBAP
+
+    -- ** Ambisonics
+
+    -- ** Binaural / HTRF
+
+) where
+
+
+
