460e66de37829251f0ee5a7ee131828b2b686c41
Anton Kholomiov <holomevao@quarta.su>
2013-01-29 12:54:44 +0400

    Math opcodes toc

---
+++ src/Csound/Opcode/Math.hs
@@ -0,0 +1,25 @@
+-- | Math
+module Csound.Opcode.Math (
+    -----------------------------------------------------
+    -- * Mathematical Calculations
+
+    -- ** Arithmetic Operations
+
+    -- ** Trigonometric Functions
+
+    -- ** Logic Operators
+
+    -----------------------------------------------------
+    -- * Converters
+
+    -- ** MIDI To Frequency
+
+    -- ** Frequency To MIDI
+
+    -- ** Cent Values To Frequency 
+
+    -- ** Amplitude Converters
+
+    -- ** Scaling
+
+) where
