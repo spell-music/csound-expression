ad4a76655a9ebe4393b312f08f5cc7cbe282346e
Anton Kholomiov <holomevao@quarta.su>
2013-01-29 12:43:48 +0400

    Interaction opcodes toc

---
+++ src/Csound/Opcode/Interaction.hs
@@ -0,0 +1,33 @@
+-- | Realtime Interaction
+module Csound.Opcode.Interaction (
+
+    -----------------------------------------------------
+    -- * MIDI
+
+    -- ** Opcodes For Use In MIDI-Triggered Instruments
+
+    -- ** Opcodes For Use In All Instruments
+
+    -----------------------------------------------------
+    -- * Open Sound Control and Network
+
+    -- ** Open Sound Control
+
+    -- ** Remote Instruments
+
+    -- ** Network Audio
+
+    -----------------------------------------------------
+    -- * Human Interfaces
+
+    -- ** Widgets
+
+    -- ** Keys
+
+    -- ** Mouse
+
+    -- ** WII
+
+    -- ** P5 Glove
+
+) where
