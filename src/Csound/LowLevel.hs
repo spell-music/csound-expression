9062a1a7c35c6f56f6a83129abd1b211644724dc
Anton Kholomiov <holomevao@quarta.su>
2013-02-07 16:41:20 +0400

    implement opcodes with LowLevel

--- src/Csound/LowLevel.hs
+++ src/Csound/LowLevel.hs
@@ -2,7 +2,7 @@
 -- You can find a lot of examples in source code (see directory @Csound/Opcode@)
 module Csound.LowLevel(
     -- * Types    
-    Val, Rate(..), Name, E, toE,
+    Val, Rate(..), Name, E, 
 
     -- * Types
     Sig, D, I, S, Tab, Spec, CsdTuple, SE, Msg,
@@ -16,8 +16,8 @@ module Csound.LowLevel(
     --
     -- > oscil :: Sig -> Sig -> Tab -> Sig 
     -- > oscil = opc3 "oscil" [
-    -- >     (Ar, [Xr, Xr, Ir, Ir]),
-    -- >     (Kr, [Kr, Kr, Ir, Ir])]
+    -- >     (a, [x, x, i, i]),
+    -- >     (k, [k, k, i, i])]
 
     Spec1, 
     opcs, opc0, opc1, opc2, opc3, opc4, opc5, opc6, opc7, opc8, opc9, opc10, opc11,
@@ -26,12 +26,12 @@ module Csound.LowLevel(
     -- | Examples:
     --
     -- > pan2 :: Sig -> Sig -> (Sig, Sig)
-    -- > pan2 = mopc2 "pan2" ([Ar, Ar], [Ar, Xr, Ir])
+    -- > pan2 = mopc2 "pan2" ([a, a], [a, x, i])
     --
     -- When you don't want to specify precise number of outputs:
     --
     -- > soundin :: CsdTuple a => S -> a
-    -- > soundin = mopc1 "soundin" (repeat Ar, Sr : replicate 4 Ir)
+    -- > soundin = mopc1 "soundin" (repeat a, s : is 4)
 
     Specs,
     mopcs, mopc0, mopc1, mopc2, mopc3, mopc4, mopc5, mopc6, mopc7,
@@ -41,10 +41,29 @@ module Csound.LowLevel(
     -- | Examples:
     --
     -- > delayr :: D -> SE Sig
-    -- > delayr a1 = se $ opc1 "delayr" [(Ar, [Ir])] a1
+    -- > delayr a1 = se $ opc1 "delayr" [(a, [i])] a1
     -- > 
     -- > delayw :: Sig -> SE ()
-    -- > delayw a1 = se_ $ opc1 "delayw" [(Xr, [Ar])] a1
+    -- > delayw a1 = se_ $ opc1 "delayw" [(x, [a])] a1
+    --
+    -- Functions that produce no values (procedures) should return value of the type "Xr".
+
+    -- * When standard functions are not enough
+    
+    -- | Sometimes Csound opcodes take too many parameters. If you want to 
+    -- use them, you can always use functions that are defined on lists ("opcs" or "mopcs").
+    -- But in this case you have to convert all arguments to the same type "E":
+    --
+    -- For example:
+    --
+    -- > oscil :: Sig -> Sig -> Tab -> Sig 
+    -- > oscil a1 a2 a3 = opcs "oscil" signature [toE a1, toE a2, toE a3]
+    -- >    where signature = [
+    -- >            (a, [x, x, i, i]),
+    -- >            (k, [k, k, i, i])]
+    
+    toE,
+
     se, se_
 ) where
 
