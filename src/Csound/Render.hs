5f46a76801d4402137a42e8874c3e5240621e440
Anton Kholomiov <anton.kholomiov@gmail.com>
2013-05-04 22:25:08 +0400

    some more bloody refactoring

--- src/Csound/Render.hs
+++ src/Csound/Render.hs
@@ -1,49 +1,21 @@
-{-# Language GADTs, DeriveFunctor, DeriveFoldable #-}
 module Csound.Render(
     render    
 ) where
 
-import Control.Arrow(second)
-import Data.Default
 import Data.Monoid
 
 import Temporal.Music.Score(temp, stretch, dur, Score, Event(..), tmap, delay)
 import qualified Temporal.Music.Score as T(render)
 
 import Csound.Exp
-import Csound.Exp.Wrapper
 import Csound.Render.Pretty
 import Csound.Render.Instr
 import Csound.Render.Options
 import Csound.Render.Channel
 
-import Csound.Tfm.String
-import Csound.Tfm.Tab
-
-import Csound.Exp.Arg
-import Csound.Exp.Tuple
+import Csound.Exp.Tuple(Out)
 import Csound.Render.InstrTable
 import Csound.Exp.Mix
-
-un = undefined
-
-rescale :: Score (Mix a) -> Score (Mix a)
-rescale = tmap $ \e -> let factor = (eventDur e / (mixDur $ eventContent e))
-                       in  mixStretch factor (eventContent e)
-    where mixDur :: Mix a -> Double
-          mixDur x = case x of
-            Sco _ a -> dur a
-            Mix _ _ a -> dur a
-            Mid _ -> 1
-
-          mixStretch :: Double -> Mix a -> Mix a
-          mixStretch k x = case x of
-            Sco a sco -> Sco a $ stretch k sco
-            Mix ar a sco -> Mix ar a $ rescale $ stretch k sco
-            Mid _ -> x     
-
-getLastInstrId :: MixerTab a -> Int
-getLastInstrId = fst . masterInstr
 
 render :: (Out a) => CsdOptions -> Score (Mix a) -> IO String
 render opt a' = do
@@ -69,7 +41,8 @@ render opt a' = do
         (ppTotalDur (dur a) $$ ppMapTable ppTabDef tabs)
     where a = rescale a'
           totalDur = dur a'
-          
+
+
 alwayson totalDur instrId = ppNote instrId 0 totalDur []      
 
 renderSnd :: InstrTab E -> Doc
@@ -88,9 +61,25 @@ renderSco formNote a = ppSco $ renderNote =<< T.render a
               MixerNote n     -> return $ formNote n (fmap (const []) e) chnVar
               SoundNote n sco -> fmap (\x -> formNote n x chnVar) $ T.render $ delay (eventStart e) sco -- only delay, stretch was done before
               MidiNote _      -> mempty
-
               
 lastInstrNotes :: Double -> (InstrId, MixerExp) -> Doc
 lastInstrNotes totalDur (instrId, a) = alwayson totalDur instrId $$ sco
     where sco = renderSco (\n evt var -> ppMasterNote n evt) $ mixerExpSco a
   
+getLastInstrId :: MixerTab a -> Int
+getLastInstrId = fst . masterInstr
+          
+rescale :: Score (Mix a) -> Score (Mix a)
+rescale = tmap $ \e -> let factor = (eventDur e / (mixDur $ eventContent e))
+                       in  mixStretch factor (eventContent e)
+    where mixDur :: Mix a -> Double
+          mixDur x = case x of
+            Sco _ a -> dur a
+            Mix _ _ a -> dur a
+            Mid _ -> 1
+
+          mixStretch :: Double -> Mix a -> Mix a
+          mixStretch k x = case x of
+            Sco a sco -> Sco a $ stretch k sco
+            Mix ar a sco -> Mix ar a $ rescale $ stretch k sco
+            Mid _ -> x     
