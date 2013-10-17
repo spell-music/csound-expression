d50da2f8c50d39902db605cf628f06e058b42524
Anton Kholomiov <anton.kholomiov@gmail.com>
2013-10-17 12:30:46 +0400

    adds unit functions to trigger the events

--- src/Csound/Control/Instr.hs
+++ src/Csound/Control/Instr.hs
@@ -6,28 +6,20 @@ module Csound.Control.Instr(
 
     -- * Midi
     Msg, Channel, midi, midin, pgmidi, ampCps,
+    midi_, midin_, pgmidi_,
     -- ** Reading midi note parameters
     cpsmidi, ampmidi,
 
     -- * Evt            
     trig, sched, schedHarp, autoOff,
+    trig_, sched_
 ) where
 
-import Csound.Typed hiding (midi, pgmidi)
-import qualified Csound.Typed as T(midi, pgmidi)
+import Csound.Typed
 import Csound.Typed.Opcode
 
 --------------------------------------------------------------------------
 -- midi
-
-midi :: Sigs a => (Msg -> SE a) -> a
-midi = midin 0
-
-midin :: Sigs a => Channel -> (Msg -> SE a) -> a
-midin chn instr = T.midi chn instr
-
-pgmidi :: Sigs a => Maybe Int -> Channel -> (Msg -> SE a) -> a
-pgmidi mn chn instr = T.pgmidi mn chn instr
 
 ampCps :: Msg -> (D, D)
 ampCps msg = (ampmidi msg 1, cpsmidi msg)
