5c39421a58a3867a844a3382f010a4f1d4ac9ad4
anton-k <anton.kholomiov@gmail.com>
2016-08-10 00:29:11 +0300

    cabbage: introduce

---
+++ src/Csound/Cabbage.hs
@@ -0,0 +1,18 @@
+module Csound.Cabbage(
+    Cab, cabbage,
+
+    -- * Widgets
+    button, filebutton, infobutton, checkbox, combobox, csoundoutput, encoder, gentable, 
+    hrange, vrange, form, groupbox, image, keyboard, label, hslider, vslider,
+    rslider, soundfiler, signaldisplay, textbox, texteditor, xypad,
+
+    -- * Properties
+    bounds, channel, text1, text2, value, colour, colour0, colour1, backgroundcolour, textcolour, trackercolour, outlinecolour, 
+    fontcolour, fontcolour0, fontcolour1, latched, identchannel, rotate, alpha, visible, caption, widgetarray, popuptext, 
+    active, svgfile, populate, mode, file, shape, corners, channeltype, align, sliderincr, max, min, textbox', trackerthickness,
+    linethickness, range, range2, size, pluginid, guirefresh, plant, child, show, middlec, keywidth, scrollbars, fontstyle,
+    scrubberpos, zoom, displaytype, updaterate, wrap
+) where
+
+import Prelude hiding (show, min, max)
+import Csound.Typed.Gui.Cab
