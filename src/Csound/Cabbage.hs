0a3fd4c006b121eeb3d31319d929ad4fa9522cd3
anton-k <anton.kholomiov@gmail.com>
2016-11-30 13:15:46 +0300

    docs for cabbage

--- src/Csound/Cabbage.hs
+++ src/Csound/Cabbage.hs
@@ -1,3 +1,30 @@
+-- | The Cab is a monad for Cabbage markup language. 
+-- The markup description can be constructed in the same way as blaze-html markup.
+--
+-- We use monadic equencing for sequencing of markup elements.
+--
+-- An example:
+--
+-- > import Csound.Base
+-- > import qualified Csound.Cabbage as C
+-- > 
+-- > ui = do
+-- >     C.cabbage $ do
+-- >         C.form $ do
+-- >            C.size 100 100
+-- >            C.pluginid "plugin"
+-- >        C.button $ do
+-- >            C.bounds 10 10 80 80
+-- >            C.channel "button"
+-- >            C.text1 "Click me"
+-- >             C.colour0 (C.Rgb 150 30 0)
+-- >             C.colour1 (C.Rgb 30 150 12) 
+-- >     res <- chnCtrlGet "button"  
+-- >     return res
+-- > 
+-- > main = dac $ do
+-- >    btn <- ui
+-- >    return $ btn * osc 220
 module Csound.Cabbage(
     Cab, CabProp, Col(..), cabbage,
 
