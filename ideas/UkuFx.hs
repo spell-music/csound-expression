5f5d9879bb29ce72a388a741ca31a2e3dc8f6bc1
anton-k <anton.kholomiov@gmail.com>
2017-03-22 21:07:46 +0300

    adds UkuFx example

---
+++ ideas/UkuFx.hs
@@ -0,0 +1,12 @@
+import Csound.Base
+
+main = run proc
+
+run = dacBy (setAdc <> setJack "fx" <> setRates 44100 32 <> setBufs 64 32)
+
+proc :: Sig2 -> Source Sig2
+proc (a1, a2) = fxApply fx a1
+    where         
+        fx = fxGridMS 4 [uiTort1m, uiFlan1, uiPhasy2, uiAdele2 0.4 0.35] def [uiChory2, uiHall2, uiGain 0.6]
+
+
